c268d32ef1a123a14145a72c4040b62f6ba4d54f
nhoward712 <Nhoward72@yahoo.com>
2020-09-20 15:50:06 -0500

    Initial commit

---
+++ src/main/java/edu/wctc/distjavazodiac/entity/Fortune.java
@@ -0,0 +1,23 @@
+package edu.wctc.distjavazodiac.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "Fortune")
+public class Fortune {
+
+    @Id
+    @Column(name = "fortune_Id")
+    private int id;
+
+    @Column(name = "text")
+    private String text;
+}
---
+++ src/main/java/edu/wctc/distjavazodiac/service/RandomHoroscopeService.java
@@ -0,0 +1,71 @@
+package edu.wctc.distjavazodiac.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.wctc.distjavazodiac.entity.Birthday;
+import edu.wctc.distjavazodiac.entity.Fortune;
+import edu.wctc.distjavazodiac.entity.Horoscope;
+import edu.wctc.distjavazodiac.repo.FortuneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Random horoscopes from https://cafeastrology.com/dailyhoroscopesall-tomorrow.html
+ */
+@Service
+public class RandomHoroscopeService implements HoroscopeService {
+//    private List<Fortune> allFortunes;
+    List<Fortune> allFortunes = initHoroscopes();
+    private ZodiacService zodiacService;
+
+    @Autowired
+    FortuneRepository fortuneRepository;
+
+    @Autowired
+    public RandomHoroscopeService(ZodiacService zodiacService) {
+        this.zodiacService = zodiacService;
+    }
+
+    @Override
+    public Horoscope getHoroscope(Birthday birthday) {
+        String sign;
+        if (birthday.getZodiacType().toLowerCase().startsWith("w")) {
+            sign = zodiacService.getWesternZodiacSign(birthday);
+        } else {
+            sign = zodiacService.getEasternZodiacSign(birthday);
+        }
+
+        Horoscope hscope = new Horoscope();
+        hscope.setSign(sign);
+
+        int randomIndex = (int) (Math.random() * allFortunes.size());
+        hscope.setHoroscope(allFortunes.get(randomIndex).getText());
+
+        return hscope;
+    }
+    @PostConstruct
+    public List<Fortune> initHoroscopes(){
+        List<Fortune> allFortunes = new ArrayList<>();
+        fortuneRepository.findAll().forEach(allFortunes::add);
+        return allFortunes;
+    }
+//    @PostConstruct
+//    public void initHoroscopes() {
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        Fortune[] fortuneArray;
+//        try {
+//            fortuneArray = mapper.readValue(Paths.get("fortunes.json").toFile(), Fortune[].class);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            fortuneArray = new Fortune[]{new Fortune()};
+//        }
+//        allFortunes = Arrays.asList(fortuneArray);
+//    }
+}
